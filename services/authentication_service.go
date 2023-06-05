e2dc1d3e1792b2e7903bb598e74ec092885e698b
charlesDinneya <dinneyacharles007@gmail.com>
2023-06-05 19:59:03 +0200

    wait group

--- services/authentication_service.go
+++ services/authentication_service.go
@@ -1,10 +1,12 @@
 package services
 
 import (
+	"log"
 	"ropc-service/authenticators"
 	"ropc-service/model/dto"
 	"ropc-service/model/entities"
 	"ropc-service/utils"
+	"sync"
 )
 
 type AuthenticatorContract interface {
@@ -25,17 +27,42 @@ func InstantiateAuthenticator(uA authenticators.UserAuthenticatorContract, cA au
 
 func (selfC Authenticator) Authenticate(user *entities.User, client *entities.Client) (*dto.Token, error) {
 
-	u, err := selfC.userAuthenticator.Authenticate(user.Username, user.Password)
-	if err != nil {
-		return nil, err
+	var wg sync.WaitGroup
+	wg.Add(2)
+	var error2 error
+	var verifiedUser *entities.User
+	var verifiedClient *entities.Client
+
+	go func() {
+		defer wg.Done()
+		u, err := selfC.userAuthenticator.Authenticate(user.Username, user.Password)
+		if err != nil {
+			error2 = err
+			return
+		}
+		verifiedUser = u
+
+	}()
+
+	go func() {
+		defer wg.Done()
+		c, err := selfC.clientAuthenticator.Authenticate(client.ClientId, client.ClientSecret)
+		if err != nil {
+			error2 = err
+			return
+		}
+		verifiedClient = c
+	}()
+
+	wg.Wait()
+
+	log.Println("er", error2)
+
+	if error2 != nil {
+		return nil, error2
 	}
 
-	c, err := selfC.clientAuthenticator.Authenticate(client.ClientId, client.ClientSecret)
-	if err != nil {
-		return nil, err
-	}
-
-	accessToken, err := utils.GenerateToken(u, c)
+	accessToken, err := utils.GenerateToken(verifiedUser, verifiedClient)
 	if err != nil {
 		return nil, err
 	}
